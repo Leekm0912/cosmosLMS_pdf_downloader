b04f4d31e48994021113de4af5c4507d6093e152
leekm <skatjrdndqo@naver.com>
2021-10-04 17:26:06 +0900

    썸네일 클릭 및 이미지 다운로드 부분 구현

--- main.py
+++ main.py
@@ -9,76 +9,68 @@ from time import sleep
 
 # ini파일 읽어오기
 cf = configparser.ConfigParser()
-cf.read("config.ini",encoding="utf-8")
-search = cf['DEFAULT']['SEARCH']
+cf.read("config.ini", encoding="utf-8")
 chrome_driver_path = cf['DEFAULT']['CHROME_DRIVER_PATH']
-img_class_name =  cf['DEFAULT']['GOOGLE_IMG_CLASSNAME']
-save_path =  cf['DEFAULT']['SAVA_PATH']
+save_path = cf['DEFAULT']['SAVE_PATH']
 
 # 검색할 url 조합
 with open("download_list", "r") as r:
     url = r.readlines()
 
-with open("copy.html", "r") as r:
-    html_read = r.read()
-
 print(url)
 
 # 셀레니움 시작
-#driver = webdriver.Chrome(chrome_driver_path)
-#driver.get(url[0])
-#driver.implicitly_wait(10)
-#driver.switch_to.frame("docFrame")
+driver = webdriver.Chrome(chrome_driver_path)
+driver.get(url[0])
+driver.implicitly_wait(10)
+sleep(3)
+driver.switch_to.frame("docFrame")
+sleep(3)
 # 파싱 시작
-#html = driver.page_source
-soup = BeautifulSoup(html_read, 'html.parser')
-
-#img = soup.find("#contents")
-#print(soup)
-
-for i in range(20):
-    try:
-        img = soup.select_one("#page" + str(i))
-        print(img.attrs["src"])
-    except AttributeError as e:
-        print(e)
-
-'''
-# 구글의 이미지 태그 대입
-img = soup.select(img_class_name)
-# 파싱 결과 확인
-print(img)
-
-# url을 담을 리스트
-imgurl = []
-
-# 반복문 돌면서 이미지의 src 추출
-# 이미지 경로가 src 또는 data-src로 되어있음
-for i in img:
-    try:
-        imgurl.append(i.attrs["src"])
-    except KeyError:
-        imgurl.append(i.attrs["data-src"])
-
-print("검색된 이미지 개수 :",len(imgurl))
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+with open("save.html", "w", encoding="utf-8") as w:
+    w.write(html)
 
 # 저장 경로 설정
-dir_path = save_path + "/" + search
+title = soup.select_one(".fnm").text
+print(title)
+dir_path = save_path + "/" + title
 
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
 
-# 다운받은 사진 url을 담은 txt파일과, 이미지파일 저장
-with open(dir_path + "/ImageURL_"+search+".txt","a") as a:
-    n=1
-    # imgurl 리스트에 든 이미지들 다운받아줌
-    for i in imgurl:
-        try:
-            urlretrieve(i,"downloads/"+search+"/"+search+str(n)+".jpg")
-            print(str(n)+"번 이미지 다운로드중")
-            a.write(str(n)+"번사진:"+i+"\n")
-            n += 1
-        except:
-            print(n,"번 이미지 다운로드중 오류 발생. 건너뜁니다")
+# 이미지 경로를 저장할 리스트
+img_list = []
+
+for i in range(1000):
+    try:
+        thumb_id = soup.select_one("#thumb" + str(i)).attrs["id"]
+        print(thumb_id)
+        # 썸네일을 눌러야 이미지가 로딩되는 형식이라 썸네일을 하나하나 눌러줌.
+        driver.find_element_by_id(thumb_id).click()
+        sleep(1)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        # 이미지가 들어있는 div 파싱
+        page = soup.select_one("#page" + str(i))
+        print("https://doc.coursemos.co.kr" + page.attrs["src"])
+        img_list.append("https://doc.coursemos.co.kr" + page.attrs["src"])
+    except AttributeError as e:
+        # 오류가 났다는건 더 이상 불러올 이미지가 없다는 뜻.
+        break
+
+# 파싱 결과 확인
+print(img_list)
+
+# 이미지파일 저장
+for i in range(len(img_list)):
+    urlretrieve(img_list[i], f"downloads/{title}/{title}_{i}.jpg")
+    print(str(i) + "번 이미지 다운로드중")
+
+# 이미지파일 경로 백업
+with open(f"downloads/{title}/image_url.txt", "w") as w:
+    w.write(str(img_list))
+
 # 드라이버 해제
-driver.close()'''
+driver.close()
