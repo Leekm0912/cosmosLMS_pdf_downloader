7f231c14f08c144992785248529b3914ef320420
Lee Kyung-min <42348176+Leekm0912@users.noreply.github.com>
2021-10-13 10:52:49 +0900

    다운받은 이미지를 pdf로 합쳐줌.
    다운받은 이미지를 pdf로 합쳐줌.

--- main.py
+++ main.py
@@ -5,16 +5,36 @@ import configparser
 from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import img2pdf
+
+
+def makePDF(src_dir, data_path=None):
+    if not data_path:
+        data_path = []
+        path = f"./downloads/{src_dir}"
+        for file in os.listdir(path):
+            if file.endswith(".png"):
+                data_path.append(file)
+        data_path.sort(key=lambda x: int(x.split("_")[1].split(".")[0]))
+        print("data_path", data_path)
+
+    load_image = []
+    for img in data_path:
+        with open(f"{path}/{img}", 'rb') as f:
+            load_image.append(f.read())
+
+    with open(f"downloads/{src_dir}/{src_dir}.pdf", "wb") as pdf_file:
+        pdf_file.write(img2pdf.convert(load_image))
+
 
 # ini 파일 읽어오기
 cf = configparser.ConfigParser()
 cf.read("config.ini", encoding="utf-8")
 chrome_driver_path = cf['DEFAULT']['CHROME_DRIVER_PATH']
 save_path = cf['DEFAULT']['SAVE_PATH']
-download_list = cf['DEFAULT']['DOWNLOAD_LIST']
 
 # 검색할 url 불러오기
-with open(download_list, "r") as r:
+with open("download_list", "r") as r:
     url_list = r.readlines()
 
 print(url_list)
@@ -25,11 +45,14 @@ for url in url_list:
     driver.get(url.strip("\n"))
     driver.implicitly_wait(10)
     sleep(3)
+    # 이미지가 frame안에 들어있어서 switch_to_frame으로 프레임 안으로 이동
     driver.switch_to.frame("docFrame")
     sleep(3)
     # 파싱 시작
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
+    with open("save.html", "w", encoding="utf-8") as w:
+        w.write(html)
 
     # 저장 경로 설정
     # pdf 문서의 제목 파싱
@@ -69,14 +92,20 @@ for url in url_list:
     # 파싱 결과 확인
     print(img_list)
 
+    img_save_path = []
     # 이미지파일 저장
     for i in range(len(img_list)):
-        urlretrieve(img_list[i], f"downloads/{title}/{title}_{i}.jpg")
         print(str(i) + "번 이미지 다운로드중")
+        img_path = f"downloads/{title}/{title}_{i}.png"
+        urlretrieve(img_list[i], img_path)
+        img_save_path.append(img_path)
 
     # 이미지파일 경로 백업
     with open(f"downloads/{title}/image_url.txt", "w") as w:
         w.write(str(img_list))
+
+    # pdf 생성
+    makePDF(title)
 
     # 드라이버 해제
     driver.close()
